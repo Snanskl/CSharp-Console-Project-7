474e5fecffb228c044c1e31ac61692a78d4eb6cd
Minou <118366099+Snanskl@users.noreply.github.com>
2023-11-26 20:46:59 +0100

    Add files via upload

---
+++ Assignment 7/ProductType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_7
+{
+    public enum ProductType //Done
+    {
+        //An enum class for product type accessed by BeautyAndDate
+        Makeup,
+        Skincare
+    }
+}
